53c91713fde72733b9a2434a06e0a76eb54b84d8
Thanh Nguyen <thanhnguyen@kaist.ac.kr>
2026-05-05 06:30:17 +0900

    update script

--- script/seed_gpu1.sh
+++ script/seed_gpu1.sh
@@ -1,4 +1,4 @@
 for seed in 0 1 2 3 4 5 6 7 8
 do
-    python script/run.py --config-dir=cfg/gym/finetune/hopper-v2 --config-name=ft_ppo_reflow_mlp.yaml device=cuda:0 train.trust_region_mode=spo train.spo_clip_coef=0.1 seed=$seed
+    python script/run.py --config-dir=cfg/gym/finetune/hopper-v2 --config-name=ft_ppo_reflow_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.1 seed=$seed
 done
