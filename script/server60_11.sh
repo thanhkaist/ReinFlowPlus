5a24a5067938cef943398ea4fdbaa566afd53c6f
Thanh Nguyen <thanhnguyen@kaist.ac.kr>
2026-05-05 07:12:06 +0900

    add script

---
+++ script/server60_11.sh
@@ -0,0 +1,25 @@
+
+python script/run.py --config-dir=cfg/gym/finetune/hopper-v2 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+python script/run.py --config-dir=cfg/gym/finetune/walker2d-v2 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+# ant-v2
+
+python script/run.py --config-dir=cfg/gym/finetune/ant-v2 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+# ant-v0
+python script/run.py --config-dir=cfg/gym/finetune/ant-v0 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+# Humanoid-v3
+python script/run.py --config-dir=cfg/gym/finetune/Humanoid-v3 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+# kitchen-complete-v0
+python script/run.py --config-dir=cfg/gym/finetune/kitchen-complete-v0 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+# kitchen-mixed-v0
+python script/run.py --config-dir=cfg/gym/finetune/kitchen-mixed-v0 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05   
+
+# kitchen-partial-v0
+python script/run.py --config-dir=cfg/gym/finetune/kitchen-partial-v0 --config-name=ft_ppo_shortcut_mlp.yaml device=cuda:1 train.trust_region_mode=spo train.spo_clip_coef=0.05
+
+
